ebb87a23cb35afb480b7d4b73dd4d39b72c05eaf
powr <powr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-13 20:21:04 +0000

    Updates
    git-svn-id: https://plugins.svn.wordpress.org/powr-twitter-feed/trunk@1180388 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- powr-twitter-feed.php
+++ powr-twitter-feed.php
@@ -1,14 +1,14 @@
 <?php
     /**
      * @package POWr Twitter Feed
-     * @version 1.4.004
+     * @version 1.4.005
      */
     /*
     Plugin Name: POWr Twitter Feed
     Plugin URI: http://www.powr.io
     Description: Showcase any Twitter @handle or #hashtag!  Drop the widget anywhere in your theme. Or use the POWr icon in your WP text editor to add to a page or post. Edit on your live page by clicking the settings icon. More plugins and tutorials at POWr.io.
     Author: POWr.io
-    Version: 1.4.004
+    Version: 1.4.005
     Author URI: http://www.powr.io
     */
 
